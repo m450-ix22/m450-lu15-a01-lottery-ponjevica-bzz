8ffb485f74f38be621875772652eb35e27f7ec76
ponjevica-bzz <ponjevica@bzz.ch>
2024-12-05 12:41:11 +0100

    Tests money

--- test_money.py
+++ test_money.py
@@ -4,15 +4,15 @@ from person import Person
 
 def test_transfer_money_deposit(monkeypatch):
     person = Person('Inga', 'geheim', 10.00)
-    inputs = iter(['E', '20.00', 'N', 'Z'])  # Cover deposit, end, and possible menu exit
+    inputs = iter(['E', '20.00', 'N', 'Z'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     transfer_money(person)
-    assert person.balance == 30.00  # Assuming deposit is successful
+    assert person.balance == 30.00
 
 
 def test_transfer_money_withdrawal(monkeypatch):
     person = Person('Inga', 'geheim', 20.00)
-    inputs = iter(['A', '12.00', 'N', 'Z'])  # Cover withdrawal, end, and possible menu exit
+    inputs = iter(['A', '12.00', 'N', 'Z'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     transfer_money(person)
-    assert person.balance == 8.00  # Assuming withdrawal is successful
+    assert person.balance == 8.00
